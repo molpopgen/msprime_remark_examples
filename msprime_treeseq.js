0dd25ad7f5789b6137ae4afd10f33c10bcfb5c38
Kevin R. Thornton <krthornt@uci.edu>
2022-05-27 13:25:00 -0700

    load pyodide, etc., once, pass to worker functions

--- msprime_treeseq.js
+++ msprime_treeseq.js
@@ -1,17 +1,10 @@
-async function local_fn() {
-          let pyodide = await loadPyodide({indexURL: "https://cdn.jsdelivr.net/pyodide/dev/full/"});
-        await pyodide.loadPackage("msprime");
-          // Pyodide is now ready to use...
-          //let msprime = await pyodide.pyimport("msprime");
-          //ts = msprime.sim_ancestry(5);
-          svg = pyodide.runPython(`
+async function msprime_treeseq(pyodide) {
+    svg = pyodide.runPython(`
               import msprime
               ts = msprime.sim_ancestry(5)
               ts.draw_svg(size=(500, 500))
               `)
-            //var where = d3.select("#treeseq_svg").append(svg)
-        let location = document.getElementById("treeseq_svg");
-        location.outerHTML += svg;
-          };
-local_fn();
+    let location = document.getElementById("treeseq_svg");
+    location.outerHTML += svg;
+};
 
